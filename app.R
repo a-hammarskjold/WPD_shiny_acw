baced4895745024917f42a4c7b1c2f73b51f773e
a-hammarskjold <77897931+a-hammarskjold@users.noreply.github.com>
2021-09-23 21:03:24 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -1,4 +1,52 @@
 library(shiny)
+library(viridis)
+library(rgdal)
+
+fillmap2<-function(map, figtitle, y , leg.loc="beside", y.scl=NULL,
+                   main.cex=1.5,main.line=0,map.lty=1,leg.rnd=0,
+                   leg.cex=1){
+  
+  # 0: dark 1: light light Current shading ranges from darkest to light gray white (to distinguish with lakes).
+  y.uq=sort(unique(c(y,y.scl)))
+  cols<-viridis(length(y.uq),direction=-1)
+  shading=y
+  for (i in 1:length(y)){
+    shading[i]<-cols[which(y.uq==y[i])]
+  }
+  
+  par(mar=c(0,0,2,0))
+  if (leg.loc=="beside"){
+    layout(matrix(1:2,ncol=2),width=c(.8,.2))
+  } else 
+    if (leg.loc=="below"){
+      layout(matrix(1:2,nrow=2),height=c(.6,.4))
+    } else (print("leg.loc options are below or beside"))
+  
+  plot(map,col=shading,axes=F, lty=map.lty)
+  title(main=figtitle,cex.main=main.cex,line=main.line) 
+  
+  par(mar=c(5, 4, 4, 2) + 0.1)
+  plot(c(0,2),c(0,1),type = 'n', axes = F,xlab = '', ylab = '', main = '')
+  cols.5=cols[seq(1,length(y.uq),length.out=5)]
+  lab.5=cols.5
+  for (i in 1:5){lab.5[i]=y.uq[which(cols==cols.5[i])[1]]}
+  lab.5=round(as.numeric(lab.5),leg.rnd)
+  par(mar=c(0,0,0,0))
+  if (leg.loc=="beside"){
+    legend_image <- as.raster(matrix(cols, ncol=1))
+    text(x=1.6, 
+         y = seq(0,length(y.uq),length.out=5)/length(y.uq),
+         labels = rev(lab.5), cex=leg.cex)
+    rasterImage(legend_image, 0, 0, 1,1)
+  } else{
+    legend_image <- as.raster(matrix(cols, nrow=1))
+    text(y=-0.25, 
+         x = seq(0,length(y.uq),length.out=5)/(length(y.uq)*.5),
+         labels = lab.5, cex=leg.cex)
+    rasterImage(legend_image, 0, 0, 2,1)
+  }
+}
+
 
 data = read.csv("Data\\shiny_data.csv")
 NHtracts = readOGR("Data\\NHtracts\\NHtracts.shp")
